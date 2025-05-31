800f74b8e98d73d376cb833f68d0b9c34312b5cb
hdzhi <ana.gadzhieva@yandex.ru>
2025-05-31 14:22:06 +0300

    Add files via upload

---
+++ OOP_Car_Game/include/View/AboutView.h
@@ -0,0 +1,24 @@
+#ifndef ABOUT_VIEW_H
+#define ABOUT_VIEW_H
+
+#include <FL/Fl_Window.H>
+#include <FL/Fl_Box.H>
+#include <FL/Fl_Button.H>
+
+class AboutController;
+
+class AboutView : public Fl_Window
+{
+private:
+    AboutController *m_pController;
+    Fl_Box *m_pTitleBox;
+    Fl_Box *m_pInfoBox;
+    Fl_Button *m_pBackButton;
+
+public:
+    AboutView(int width, int height, const char *title, AboutController *controller);
+
+    static void backCallback(Fl_Widget *widget, void *data);
+};
+
+#endif // ABOUT_VIEW_H
---
+++ OOP_Car_Game/include/View/GameView.h
@@ -0,0 +1,32 @@
+#ifndef GAME_VIEW_H
+#define GAME_VIEW_H
+
+#include <FL/Fl_Window.H>
+#include <FL/Fl_Box.H>
+#include <FL/Fl_Button.H>
+#include <string>
+
+class GameController;
+
+class GameView : public Fl_Window
+{
+private:
+    GameController *m_pController;
+    Fl_Box *m_pScoreBox;
+    Fl_Box *m_pHighScoreBox;
+    Fl_Box *m_pGameOverBox;
+    Fl_Button *m_pBackButton;
+
+public:
+    GameView(int width, int height, const char *title, GameController *controller);
+
+    void draw() override;
+    void updateScore(int score);
+    void updateHighScore(int highScore);
+    void showGameOver();
+    void hideGameOver();
+
+    static void backToMenuCallback(Fl_Widget *widget, void *data);
+};
+
+#endif // GAME_VIEW_H
