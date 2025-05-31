e9162cf0b5df2973dc86e69c3b3544d7f1f95e32
hdzhi <ana.gadzhieva@yandex.ru>
2025-05-31 14:21:12 +0300

    Add files via upload

---
+++ OOP_Car_Game/include/Controller/AboutController.h
@@ -0,0 +1,21 @@
+#ifndef ABOUT_CONTROLLER_H
+#define ABOUT_CONTROLLER_H
+
+#include "View/AboutView.h"
+
+class MainMenuController;
+
+class AboutController
+{
+private:
+    AboutView m_view;
+    MainMenuController *m_pMainMenuController;
+
+public:
+    AboutController(int width, int height, MainMenuController *mainMenuController);
+
+    void showAbout();
+    void backToMenu();
+};
+
+#endif // ABOUT_CONTROLLER_H
---
+++ OOP_Car_Game/include/Controller/GameController.h
@@ -0,0 +1,30 @@
+#ifndef GAME_CONTROLLER_H
+#define GAME_CONTROLLER_H
+
+#include "View/GameView.h"
+#include "Model/GameModel.h"
+#include <FL/Fl.H>
+
+class MainMenuController;
+
+class GameController
+{
+private:
+    GameModel m_model;
+    GameView m_view;
+    MainMenuController *m_pMainMenuController;
+
+    static void timerCallback(void *data);
+
+public:
+    GameController(int width, int height, MainMenuController *mainMenuController);
+
+    void startGame();
+    void backToMenu();
+    void handleKeyPress(int key);
+    void drawGame() const;
+
+    GameView &getView();
+};
+
+#endif // GAME_CONTROLLER_H
